e5202ab1c1f79d2cf1ef613ba6379a5abe1f1828
nayem-ahmedz <nayemahmed2n@gmail.com>
2025-09-03 21:53:07 +0600

    added more in test

--- practice/milestones/m6/module-32/API/script.js
+++ practice/milestones/m6/module-32/API/script.js
@@ -1,12 +1,14 @@
+const title = document.querySelector('#title');
 function loadPosts(){
     const url = 'https://jsonplaceholder.typicode.com/posts';
     fetch(url)
         .then(res => res.json())
         .then(data => display(data));
 }
+const container = document.querySelector('#posts-container');
 function display(posts){
-    const container = document.querySelector('#posts-container');
     container.innerHTML = '';
+    title.innerText = 'All Posts';
     setTimeout(() => {
         posts.forEach(post => {
         const postCard = document.createElement('div');
@@ -19,4 +21,25 @@ function display(posts){
         container.appendChild(postCard);
     });
     }, 500);
+}
+
+//  load todos
+function loadTodos(){
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(res => res.json())
+        .then(data => displayTodos(data));
+        // .catch(error => console.log(error));
+}
+function displayTodos(todos){
+    container.innerHTML = '';
+    title.innerText = 'All Todos list';
+    todos.forEach(todo => {
+        const card = document.createElement('div');
+        card.classList.add('todo-card');
+        card.innerHTML = `
+        ${todo.completed ? '<i class="fa-regular fa-square-check"></i>' : '<i class="fa-regular fa-square"></i>'}
+        <h3>${todo.title}</h3>
+        `;
+        container.append(card);
+    })
 }
